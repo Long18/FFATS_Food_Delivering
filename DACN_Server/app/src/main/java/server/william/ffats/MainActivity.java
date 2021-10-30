a1a010288485ba1cabd4ac0859e7b38eaebf9228
WiIIiam <us.thanhlong18@gmail.com>
2021-10-30 16:17:40 +0700

    update icon

--- DACN_Server/app/src/main/java/server/william/ffats/MainActivity.java
+++ DACN_Server/app/src/main/java/server/william/ffats/MainActivity.java
@@ -1,13 +1,40 @@
 package server.william.ffats;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        txtSlogan = findViewById(R.id.txtSlogan);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, activity_sign_in.class));
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(MainActivity.this, activity_sign_up.class));
+            }
+        });
     }
 }
